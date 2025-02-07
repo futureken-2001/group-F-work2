3d155ff2ddd58cb8dd53c35019207b529265bab2
Ntale3 <swamaduntale98@gmail.com>
2025-02-07 13:31:20 +0300

    implented the required methods for the instance variables

--- src/main/java/org/example/ContactsList.java
+++ src/main/java/org/example/ContactsList.java
@@ -1,5 +1,37 @@
 package org.example;
 
 public class ContactsList {
+    private String names;
+    private String email;
+    private String number;
 
+    public ContactsList(String names, String email, String number) {
+        this.names = names;
+        this.email = email;
+        this.number = number;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
