c47fa2d0423172bf23a7823414306c6719e4d739
Ntale3 <swamaduntale98@gmail.com>
2025-02-07 12:44:27 +0300

    created instance variables to be used

--- src/main/java/org/example/ContactsWindow.java
+++ src/main/java/org/example/ContactsWindow.java
@@ -1,4 +1,21 @@
 package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class ContactsWindow {
+
+    private ArrayList<ContactsList> contactsList=new ArrayList<>();
+    private DefaultListModel<String> contactLisstModel;
+    private JFrame contacts;private JPanel northPanel,southPanel,westPanel,centerPanel,formPanel,newPanel,listPanel;
+    private  JTextField field,field2,field3;
+    private String text,text2,text3;
+    private JLabel label2,label1,label3,labelOutput1,labelOutput2,labelOutput3;
+    JList contactListview;
+    private JButton saveButton,cancelButton,EditButton,AddButton,backToListButton,deleteButton,viewButton;
+    private CardLayout cardLayout = new CardLayout();
+    actionListner al=new actionListner();
+
 }
